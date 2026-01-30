b19abb5c446ffb4eb9a143ff0ca7ca2937621c15
hsheikh776-png <hsheikh776@gmail.com>
2026-01-30 15:32:13 -0500

    Update config.js

--- config.js
+++ config.js
@@ -42,15 +42,15 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "WOOOOW You love me that much maloves?? 🥰🚀💝",  // Shows when they go past 5000%
+        high: "To infinity and beyond!  🚀💝",              // Shows when they go past 1000%
+        normal: "And beyond! 🥰 muvsie wubsies"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "Yay! I'm the luckiest person in the world! Hooray for my little Muvs! 🎉💝💖💝💓",
+        message: "Now come get your gift, a big warm hug and a huge kiss my sweet peach!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
